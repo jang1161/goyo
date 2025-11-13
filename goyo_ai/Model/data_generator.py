0168399bde9ab2b43fdecf717301a8fb24ef9b25
kimtaerimm <trnara5375@gmail.com>
2025-11-13 14:13:37 +0900

    first

---
+++ goyo_ai/Model/data_generator.py
@@ -0,0 +1,74 @@
+import numpy as np
+import librosa
+import math
+from tensorflow.keras.utils import Sequence  # Keras에서 Sequence를 임포트
+from augment_utils import add_noise, pitch_shift, mask_time, mask_freq
+
+#Keras를 위한 실시간 오디오 데이터 제너레이터
+class SoundDataGenerator(Sequence):
+    def __init__(self, file_paths, labels, batch_size, target_length, 
+                 sample_rate, class_names, augment=False):
+        self.file_paths = file_paths
+        self.labels = labels
+        self.batch_size = batch_size
+        self.target_length = target_length
+        self.sample_rate = sample_rate
+        self.class_names = class_names
+        self.augment = augment      #True면 훈련용, False면 검증용
+        self.n_classes = len(class_names)
+        self.on_epoch_end()         #한번 섞어줌
+
+    def __len__(self):
+        """한 에폭당 배치 갯수"""
+        return math.ceil(len(self.file_paths) / self.batch_size)
+
+    def __getitem__(self, index): #Keras가 model.fit에서 'index'번째 배치를 요청할 때 호출되는 함수
+        start_idx = index * self.batch_size
+        end_idx = (index + 1) * self.batch_size
+
+        # 리스트에서 [0:32] (0~31번) 파일 경로와 라벨 32개를 꺼냄
+        batch_paths = self.file_paths[start_idx:end_idx]
+        batch_labels = self.labels[start_idx:end_idx]
+        
+        # 데이터를 담을 빈 Numpy 배열 생성
+        X_batch = np.zeros((len(batch_paths), self.target_length), dtype=np.float32)  # (batch_size, 15600)
+        y_batch = np.array(batch_labels, dtype=np.int32) # (batch_size,)
+        
+        #augmentation
+        for i, (file_path, label) in enumerate(zip(batch_paths, batch_labels)):
+            try:
+                wav_data, _ = librosa.load(file_path, sr=self.sample_rate, mono=True)
+                wav_data, _ = librosa.effects.trim(wav_data, top_db=30) #시작과 끝의 '무음' 구간을 자동으로 잘라냄
+                
+                #실시간 augment
+                if self.augment:
+                    if np.random.rand() > 0.5:
+                        wav_data = add_noise(wav_data, noise_factor=np.random.uniform(0.001, 0.005))
+                    if np.random.rand() > 0.3:
+                        wav_data = pitch_shift(wav_data, self.sample_rate, n_steps=np.random.randint(-2, 3))
+                    if np.random.rand() > 0.3:
+                        wav_data = mask_time(wav_data)
+                    if np.random.rand() > 0.7:
+                        wav_data = mask_freq(wav_data, 10)
+                        
+                if len(wav_data) < self.target_length: #15600보다 짧을 때 zero-padding
+                    wav_data = np.pad(wav_data, (0, self.target_length - len(wav_data)))
+                else:
+                    max_start_index = len(wav_data) - self.target_length
+                    if max_start_index > 0:
+                        start_index = np.random.randint(0, max_start_index) 
+                    else:
+                        start_index = 0
+                    wav_data = wav_data[start_index : start_index + self.target_length] #길이가 15600보다 길면 랜덤으로 중간 어느지점을 15600만큼 자름
+                
+                X_batch[i] = wav_data
+                
+            except Exception as e:
+                print(f"error : - {e}")
+        return X_batch, y_batch
+
+    def on_epoch_end(self):# 매 에폭마다 데이터 섞어주기 위해서. + 파일 경로와 라벨을 '같은 순서로' 섞어야 함
+        indices = np.arange(len(self.file_paths)) # train file갯수만큼 인덱스 리스트
+        np.random.shuffle(indices)
+        self.file_paths = [self.file_paths[i] for i in indices]
+        self.labels = [self.labels[i] for i in indices]         #'파일 경로'와 '라벨' 리스트를 섞인 순서대로 재정렬
---
+++ goyo_ai/Model/tuning_model.py
@@ -0,0 +1,144 @@
+import tensorflow as tf
+import numpy as np
+import os
+import glob
+from sklearn.utils import class_weight
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from custom_layers import YAMNetLayer
+from data_generator import SoundDataGenerator
+
+#오디오파일을 모두 업로드하면 용량문제가 생기기 때문에, 모든 파일에 대한 'path'만 받아옴 (데이터를 RAM에 올리지 않는다.)
+def scan_dataset(dataset_path, class_names):
+    file_paths = []
+    labels = []
+    print(f"\n데이터셋 스캔 시작: {dataset_path}")
+    
+    for class_index, class_name in enumerate(class_names):
+        class_folder = os.path.join(dataset_path, class_name)
+        if not os.path.exists(class_folder):
+            print(f"경고: {class_folder} 폴더를 찾을 수 없습니다. 건너뜁니다.")
+            continue
+        paths = glob.glob(os.path.join(class_folder, '*.wav')) + \
+                glob.glob(os.path.join(class_folder, '*.mp3'))         # 모든 클래스폴더에서  .wav, .mp3 파일 검색
+        file_paths.extend(paths)
+        labels.extend([class_index] * len(paths))
+        
+    print(f"--- 스캔 완료. 총 {len(file_paths)}개 파일 경로 확보. ---")
+    return file_paths, labels
+
+SAMPLE_RATE = 16000
+AUDIO_LENGTH_SAMPLES = 15600 # YAMNet의 윈도우 크기에 맞춘 값 (0.975초)
+DATASET_PATH = '../Dataset/Final_dataset' 
+CLASS_NAMES = [
+    'Air_conditioner',
+    'Air_plane',
+    'Background',
+    'Car_horn', 
+    'Children_playing',
+    'Clock_tick',
+    'Dog_bark', 
+    'Drilling', 
+    'Engine_idling',
+    'Fan',
+    'Hair_dryer',
+    'Jackhammer', 
+    'Keyboard_typing',
+    'Rain',
+    'Refrigerator_Hum',
+    'Siren', 
+    'Snoring',
+    'Train',
+    'Vacuum',
+    'Washing_machine'
+]
+NOISE_CLASSES = len(CLASS_NAMES)
+
+# 수정본, 드롭아웃 포함
+def build_finetuned_model(NOISE_CLASSES):
+    inputs = tf.keras.layers.Input(shape=(AUDIO_LENGTH_SAMPLES,), dtype=tf.float32, name='audio_input')
+    embeddings = YAMNetLayer()(inputs) 
+    flattened_embeddings = tf.keras.layers.Flatten(name='flatten_embeddings')(embeddings)
+
+    x = tf.keras.layers.Dropout(0.5)(flattened_embeddings)  # 훈련 중에 50%의 뉴런을 무작위로 꺼서, 모델이 암기에 의존하지 못하게 함
+
+    outputs = tf.keras.layers.Dense(NOISE_CLASSES, activation='softmax', name='custom_classifier')(x)
+    
+    model = tf.keras.Model(inputs=inputs, outputs=outputs, name='yamnet_finetuned')
+    return model
+
+model = build_finetuned_model(NOISE_CLASSES)
+model.summary() # 학습시 모델 구조 확인할 것
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy', 
+    metrics=['accuracy']
+)
+
+# 데이터셋 스캔 및 분리, 훈련용과 검증용을 8:2 비율로 파일 경로 리스트를 분리 (그냥 데이터를 분리하는 것)
+all_files, all_labels = scan_dataset(DATASET_PATH, CLASS_NAMES)
+train_files, val_files, train_labels, val_labels = train_test_split(
+    all_files, 
+    all_labels, 
+    test_size=0.2, # 전체에서 20%를 검증용으로 사용
+    random_state=42, 
+    stratify=all_labels #원본의 클래스 비율을 유지하며 분리
+)
+
+print(f"총 파일: {len(all_files)} -> 훈련용: {len(train_files)}, 검증용: {len(val_files)}")
+
+# 클래스 불균형을 고려하여 훈련 데이터 라벨만 사용해서 가중치 계산
+class_weights = class_weight.compute_class_weight(
+    'balanced', #데이터 개수에 반비례하게 가중치를 줌 (적을수록 많은 가중치)
+    classes=np.unique(train_labels), # np.unique(y_train) -> train_labels
+    y=train_labels                   # y_train -> train_labels
+)
+class_weight_dict = {i : class_weights[i] for i in range(len(class_weights))}
+print(f"클래스 가중치 적용: {class_weight_dict}")
+
+
+BATCH_SIZE = 32
+# 훈련용 제너레이터
+train_generator = SoundDataGenerator(
+    file_paths=train_files,
+    labels=train_labels,
+    batch_size=BATCH_SIZE,
+    target_length=AUDIO_LENGTH_SAMPLES,
+    sample_rate=SAMPLE_RATE,
+    class_names=CLASS_NAMES,
+    augment=True
+)
+# 검증용 제너레이터
+val_generator = SoundDataGenerator(
+    file_paths=val_files,
+    labels=val_labels,
+    batch_size=BATCH_SIZE,
+    target_length=AUDIO_LENGTH_SAMPLES,
+    sample_rate=SAMPLE_RATE,
+    class_names=CLASS_NAMES,
+    augment=False
+)
+
+os.makedirs('checkpoints', exist_ok=True) #자동저장
+
+checkpoint_cb = ModelCheckpoint(
+    'checkpoints/best_model.keras',
+    monitor='val_accuracy', #'accuracy'가 아닌 'val_accuracy'(검증 정확도)를 모니터링해야 함.(전자는 과적합 우려)
+    save_best_only=True,
+    mode='max',
+    verbose=1
+)
+
+#start training
+print("\n훈련 시작")
+model.fit(
+    train_generator,
+    epochs=100,
+    batch_size=None, # 제너레이터가 배치 크기를 관리하므로 None 혹은 BATCH_SIZE
+    class_weight=class_weight_dict,
+    validation_data=val_generator, 
+    callbacks=[checkpoint_cb],
+    steps_per_epoch=len(train_generator),
+    validation_steps=len(val_generator)
+)
+print("훈련 완료")
